696352e977a5be52f1137aa1a636f145e313e682
Pradyumna Singh <pradyumnasingh2001@gmail.com>
2026-04-13 17:29:23 +0530

    second

--- config/passport.js
+++ config/passport.js
@@ -2,12 +2,22 @@ var GoogleStrategy = require("passport-google-oauth20").Strategy;
 const passport = require("passport");
 const { User } = require("../Models/user");
 
+const appBaseUrl =
+  process.env.APP_BASE_URL ||
+  process.env.BASE_URL ||
+  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "");
+
+const googleCallbackUrl =
+  process.env.GOOGLE_CALLBACK_URL ||
+  (appBaseUrl ? `${appBaseUrl}/auth/google/callback` : "") ||
+  "http://localhost:3000/auth/google/callback";
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL: googleCallbackUrl,
     },
     async function (accessToken, refreshToken, profile, cb) {
       try {
